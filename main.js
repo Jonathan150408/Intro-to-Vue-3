01d55c0c892271763ccca147917a610acbc8527b
Jonathan150408 <jonathan.junod@eduvaud.ch>
2026-01-13 15:03:10 +0100

    feat(ex 02) : finish the exercice

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-const product = 'Socks'
+// create app
+const app = Vue.createApp({
+  data() {
+    return {
+      product: "Socks",
+      description: "Basic socks, cheap and usefull",
+    };
+  },
+});
